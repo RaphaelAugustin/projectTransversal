9209949712359f1771662ad468ea41c609eca899
RaphaelAugustin <taf.augustin@gmail.com>
2015-05-28 22:13:55 +0200

    modif

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -18,17 +18,19 @@ class UserManager {
     function __construct($connection) {
         $this->db = $connection;
     }
-    function create_user($login, $pass)
+    function create_user(array $params)
     {
 
         //requete pour recupéré les info des comptes.
-        $response = $this->db->query('SELECT pseudo FROM users');
+        $response = $this->db->query('SELECT login FROM users');
 
 
         //verify if username already exist.
+
+        //C'est Sale !!!'
         while ($data = $response->fetch()) {
             foreach ($data AS $user) {
-                if ($user == $login) {
+                if ($user == $params['username']) {
                     echo "Ce nom d'utilisateur existe déjà. Veuillez en choisir un autre.";
                     exit;
                 }
@@ -36,17 +38,36 @@ class UserManager {
         }
         //cryptage password en SHA-1 (METTRE A JOUR EN SELL).
 
-        $password_crypt = sha1($pass);
+        $password_crypt = sha1($params['password']);
+
+        if ($params['sex'] = "monsieur") {
+            $gender = true;
+        } else {
+            $gender = false;
+        }
 
 
         //requete preparé et exécution pour add pseudo et password dans la db.
-        $req = $this->db->prepare("INSERT INTO users (pseudo, password) VALUES (:pseudo, :password)");
+        $req = $this->db->prepare("INSERT INTO users (login, password, gender, first_name, last_name, birthday, address, code_postal, city, country, phone_number, email, pin)
+ VALUES (:login, :password, :gender, :first_name, :last_name, :birthday, :address, :code_postal, :city, :country, :phone_number, :email, :pin)");
 
         //bind des paramètre pour que SQL fasse les vérification de validité des chaine envoyées.
 
         $req->execute([
-            'pseudo' => $login,
+            'login' => $params['username'],
             'password' => $password_crypt,
+            'gender' => $gender,
+            'first_name' => $params['prenom'],
+            'last_name' => $params['nom'],
+            'birthday' => $params['anniversaire'],
+            'address' => $params['adresse'],
+            'code_postal' => $params['code'],
+            'city' => $params['ville'],
+            'country' => $params['pays'],
+            'phone_number' => $params['telephone'],
+            'email' => $params['mail'],
+            'pin' => $params['code-pin']
+
         ]);
 
         echo "Votre compte à été crée";
